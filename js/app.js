5d8c6fafbde007effa1a522d51495aa7eb2e2c38
Tobi Wan <Tobi.Klatt@web.de>
2017-01-22 21:41:58 +0100

    Movement know functions with acceleration and resistance

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
     window.addEventListener("load", function() {
         // Variablen initialisieren
         var tickRate = 100;
-        var keyBinding = {left: 37, rigt: 39, up: 40, down: 38};
-        var gravity = -3;
+        var keyBinding = {left: 37, rigt: 39, up: 38, down: 40};
+        var gravity = 1;
         var field = (function () {
             var image = new Image();
             image.src = "media/images/background.png";
@@ -23,39 +23,36 @@
                 dimensions: {width: image.width, height: image.height}
             };
         })();
+
         var player = (function () {
             var image = new Image();
             image.src = "media/images/player.png";
 
             return {
                 image: image,
-                position: {x: 300, y: 50},
+                position: {x: field.dimensions.width/2, y: field.dimensions.height/2},
                 dimensions: {width: image.width, height: image.height},
-                speed: 5
+                maxSpeed: 10,
+                acceleration: 3,
+                resistance: 1,
+                speed: 0
             };
         })();
+
         var intervall = window.setInterval(loop, tickRate);
 
         // Event Listener für Tastendruck
         document.addEventListener("keypress", function (event) {
             switch(event.keyCode) {
                 case keyBinding.left:
-                    player.position.x -= player.speed;
-                    //loop();
                     break;
                 case keyBinding.rigt:
-                    player.position.x += player.speed;
-                    //loop();
                     break;
                 case keyBinding.down:
-                    player.position.y -= player.speed;
-                    //loop();
                     break;
                 case keyBinding.up:
-                    player.position.y += player.speed;
-                    //loop();
+                    if(player.speed < player.maxSpeed) player.speed += player.acceleration;
                     break;
-
                 break;
             }
         });
@@ -70,6 +67,18 @@
         // game loop
         function loop() {
             //gravityEffect();
+            //player.speed -= gravity;
+            //player.position.y -= player.speed;
+            //console.log(player.speed - gravity);
+            // if(player.speed > -gravity) player.speed -= gravity;
+            // player.position.y -= player.speed;
+
+
+            //if(player.speed > -gravity) player.speed -= gravity; // Gravity effects
+            if(player.speed > 0) player.speed -= player.resistance; // Resistance stops accerleration
+
+            player.position.y -= player.speed;
+
             field.canvas.clearRect(0,0,600,600);
             field.canvas.drawImage(planet.image, planet.position.x, planet.position.y);
             field.canvas.drawImage(player.image, player.position.x, player.position.y);
